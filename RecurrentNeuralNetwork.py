077a765ee4e495a747f114c17a27d0330308d000
Paolitoes <carlospaolo15@gmail.com>
2025-12-14 01:59:19 -0500

    Turning Names Into Tensors Pt 1

--- RecurrentNeuralNetwork.py
+++ RecurrentNeuralNetwork.py
@@ -11,6 +11,7 @@ if torch.cuda.is_available():
 torch.set_default_device(device)
 print(f"Using Device = {torch.get_default_device()}")
 
+
 # Preparing The Data
 
 allowed_characters = string.ascii_letters + " .,;'" + "_"
@@ -22,3 +23,18 @@ def unicodeToAscii(s):
             if unicodedata.category(c) != 'Mn'
             and c in allowed_characters
             )
+
+
+# Turning Names into Tensors
+
+def letterToIndex(letter):
+    if letter not in allowed_characters:
+        return allowed_characters.find("_")
+    else:
+        return allowed_characters.find(letter)
+
+def lineToTensor(line):
+    tensor = torch.zeros(len(line), 1, n_letters)
+    for li, letter in enumerate(line):
+        tensor[li][0][letterToIndex(letter)] = 1
+    return tensor
