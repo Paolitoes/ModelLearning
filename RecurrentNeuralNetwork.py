3466761facd640a07eb4948b433ae29fc4a79554
Paolitoes <carlospaolo15@gmail.com>
2025-12-14 01:26:16 -0500

    Preparing the Data

--- RecurrentNeuralNetwork.py
+++ RecurrentNeuralNetwork.py
@@ -1,4 +1,8 @@
 import torch
+import string
+import unicodedata
+
+# Preparing Torch
 
 device = torch.device('cpu')
 if torch.cuda.is_available():
@@ -6,3 +10,15 @@ if torch.cuda.is_available():
 
 torch.set_default_device(device)
 print(f"Using Device = {torch.get_default_device()}")
+
+# Preparing The Data
+
+allowed_characters = string.ascii_letters + " .,;'" + "_"
+n_letters = len(allowed_characters)
+
+def unicodeToAscii(s):
+    return ''.join(
+            c for c in unicodedata.normalize('NFD', s)
+            if unicodedata.category(c) != 'Mn'
+            and c in allowed_characters
+            )
